691f05c03f1f7cd40189d83c57c611a52a41ec05
Dan <dshores16@gmail.com>
2025-06-16 23:50:37 +1000

    kinda fixed the height thing

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
     <v-app :style="{background: $vuetify.theme.themes[theme].background}">
-        <v-content class="center-y">
-            <v-container fluid class="pa-1">
-                <v-row>
-                    <div height=100% class="pa-1 pl-6">
-                        <ImageViewer />
-                    </div>
-                    <div height=100% v-if="imgSrc" id="right" class="pa-1 pr-4">
-                        <v-container fluid class="px-1 py-0">
-                            <v-row>
-                                <v-col cols=12 class="px-1 pt-0 pb-1">
-                                    <ZoomPanel />
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col cols=12 class="px-1 py-0">
-                                    <RightPanel />
-                                </v-col>
-                            </v-row>
-                        </v-container>
-                    </div>
+        <v-content class="fill-height">
+            <v-container fluid class="fill-height">
+                <v-row class="fill-height" align="stretch" no-gutters>
+                    <!-- Left: ImageViewer -->
+                    <v-col class="fill-height" style="box-sizing: border-box;">
+                        <ImageViewer/>
+                    </v-col>
+                    <!-- Right: ZoomPanel + RightPanel stacked -->
+                    <v-col class="fill-height d-flex flex-column" style="box-sizing: border-box;">
+                        <div style="height:200px;">
+                            <ZoomPanel style="border-left: 12px solid rgb(34, 34, 41) !important;"/>
+                        </div>
+                        <div class="flex-grow-1 d-flex flex-column">
+                            <RightPanel style="border-top: 12px solid rgb(34, 34, 41) !important; border-left: 12px solid rgb(34, 34, 41) !important;"/>
+                        </div>
+                    </v-col>
                 </v-row>
             </v-container>
         </v-content>
